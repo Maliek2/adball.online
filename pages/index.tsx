bbe791ded415996f426f2e585fd16f1cb5d337a2
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-10-09 23:03:05 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,23 +19,6 @@ export default function IndexPage() {
       <Byline author="Maliek Galant" />
       <p className="caption">Slimey Slimey</p>
       
-        <amp-img
-        alt="Mountains"
-        width="550"
-        height="368"
-        layout="responsive"
-        src="https://www.adball.tech/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Fimg%2Fhero.308317785705ca80cdcabba1990330ca.png&w=1920&q=75"
-      >
-        <amp-img
-          alt="Mountains"
-          fallback=""
-          width="550"
-          height="368"
-          layout="responsive"
-          src="https://www.adball.tech/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Fimg%2Fhero.308317785705ca80cdcabba1990330ca.png&w=1920&q=75"
-        ></amp-img>
-      </amp-img>
-
       
 
         
