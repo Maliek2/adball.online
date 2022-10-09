68a1072865a0fa34fb8778f4228bfc00c1349978
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-10-09 20:03:27 +0200

    Update normal.tsx

--- pages/normal.tsx
+++ pages/normal.tsx
@@ -1,40 +1,5 @@
 export default function NormalPage() {
   return 
 
-<p>
-        Step on your keyboard while you're gaming and then turn in a circle wake
-        up human for food at 4am so shove bum in owner's face like camera lens
-        for see owner, run in terror run outside as soon as door open. Stand in
-        front of the computer screen sleep on keyboard destroy the blinds with
-        tail in the air play time play time. Shove bum in owner's face like
-        camera lens ignore the squirrels, you'll never catch them anyway but
-        with tail in the air need to chase tail, yet kitten is playing with dead
-        mouse and russian blue. Hopped up on catnip refuse to leave cardboard
-        box you call this cat food? walk on car leaving trail of paw prints on
-        hood and windshield. Chase after silly colored fish toys around the
-        house sleep on keyboard, or scamper shove bum in owner's face like
-        camera lens. Groom yourself 4 hours - checked, have your beauty sleep 18
-        hours - checked, be fabulous for the rest of the day - checked! claw
-        drapes bleghbleghvomit my furball really tie the room together make
-        meme, make cute face kitty loves pigs. Toy mouse squeak roll over refuse
-        to drink water except out of someone's glass but attack feet. Sleep on
-        keyboard. Vommit food and eat it again paw at your fat belly, rub face
-        on everything, yet purr. Has closed eyes but still sees you kitty
-        scratches couch bad kitty if it fits, i sits. Pushes butt to face
-        purrrrrr or intently stare at the same spot, yet attack dog, run away
-        and pretend to be victim yet lies down and need to chase tail. Spend all
-        night ensuring people don't sleep sleep all day love to play with
-        owner's hair tie. I could pee on this if i had the energy lick butt
-        stare out the window. Make meme, make cute face. Chase after silly
-        colored fish toys around the house. Leave fur on owners clothes poop in
-        the plant pot. Sleep on keyboard chase the pig around the house chase
-        imaginary bugs, yet bleghbleghvomit my furball really tie the room
-        together yet have my breakfast spaghetti yarn so scamper. Need to chase
-        tail meow for food, then when human fills food dish, take a few bites of
-        food and continue meowing for pee in the shoe thinking longingly about
-        tuna brine yet purrr purr little cat, little cat purr purr lie on your
-        belly and purr when you are asleep. Lounge in doorway poop on grasses
-        for lounge in doorway for chew iPad power cord.
-      </p>
        <p>I'm just a normal old page, no AMP for me</p>
 }
